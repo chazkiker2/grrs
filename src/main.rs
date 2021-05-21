cada69a0e65e0986aacd4605e208fd84a340a948
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-05-21 11:06:16 -0500

    add nicer error output; optimize print

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use structopt::StructOpt;
-use std::io::{self, prelude::*, BufReader};
+use anyhow::{Context, Result};
 use std::fs::File;
-use std::path::{PathBuf};
+use std::io::{self, BufReader, prelude::*, Write};
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt, Debug)]
@@ -13,21 +14,31 @@ struct Cli {
     path: PathBuf
 }
 
-fn main() -> io::Result<()> {
-    let args = Cli::from_args();
+#[derive(Debug)]
+struct CustomError(String);
 
-    // optimized solution
-    let file = File::open(args.path)?;
+fn main() -> Result<()> {
+    let args: Cli = Cli::from_args();
+
+    // try to open file
+    let path = args.path.to_owned();
+    let file = File::open(args.path)
+        .with_context(|| format!("could not read file `{}`", path.display()))?;
+
+    let stdout = io::stdout();
+    let mut handle = io::BufWriter::new(stdout);
+
+    // read file and write (to wrapped stdout) all lines which match the given pattern
     let reader = BufReader::new(file);
-
     for line in reader.lines() {
         let line = line.unwrap();
         if line.contains(&args.pattern) {
-            println!("{}", line);
+            writeln!(handle, "{}", line)?;
         }
     }
+    // print all matching lines
+    handle.flush().unwrap();
 
     Ok(())
-
 }
 
